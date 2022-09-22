8bdb218a6f72ae6e68eb1e6092c754ae46fb25a4
InstincDev <crene7722@gmail.com>
2022-09-22 09:08:09 -0500

    added dynamic profile login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,9 @@ const validator = require("validator");
 const User = require("../models/User");
 
 exports.getLogin = (req, res) => {
+   
   if (req.user) {
-    return res.redirect("user/profile");
+    return res.redirect(req.user.role+"/profile");
   }
   res.render("login", {
     title: "Login",
